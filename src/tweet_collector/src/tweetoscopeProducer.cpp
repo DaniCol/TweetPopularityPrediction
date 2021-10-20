d65eaf9767ee4434aab27a5889b3f987bea42329
guillaume barree <guillaume.barree@student-cs.fr>
2021-10-20 18:35:02 +0200

    [+] Producer class definition

---
+++ src/tweet_collector/src/tweetoscopeProducer.cpp
@@ -0,0 +1,18 @@
+#include <cppkafka/cppkafka.h>
+#include <string>
+
+#include "../include/tweetoscopeProducer.hpp"
+#include "../include/tweetoscopeCollectorParams.hpp"
+
+tweetoscope::Producer::Producer(tweetoscope::params::collector& params, std::string out_topic)
+  : cppkafka::Producer(
+      cppkafka::Configuration{
+        { "metadata.broker.list", params.kafka.brokers},
+        { "log.connection.close", false}
+      }
+    ),
+    topic(out_topic){}
+
+void tweetoscope::Producer::post_msg(std::string key, std::string msg){
+  this->produce(cppkafka::MessageBuilder(this->topic).key(key).payload(msg));
+}
