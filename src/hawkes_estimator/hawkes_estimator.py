[*] Bug Fix - Compute G1 when time gets bigger - avoid overflow

src/hawkes_estimator/hawkes_estimator.py
@@ -18,7 +18,6 @@ def main(args):
     consumer = KafkaConsumer('cascade_series',                                           # Topic name
                             bootstrap_servers = args.broker_list,                        # List of brokers passed from the command line
                             value_deserializer=lambda v: json.loads(v.decode('utf-8')),  # How to deserialize the value from a binary buffer
-                            key_deserializer= lambda v: v.decode()                       # How to deserialize the key (if any)
                             )
 
     # Init the producer
@@ -37,6 +36,7 @@ def main(args):
 
         # Get the history : [(t1,m1), (t2,m2), ....] --> np.array(n,2)
         history = np.array(v['tweets'])
+        history[:,0] -= history[0,0]
 
         # Get the current time (end of the observation window)
         t = v['T_obs']