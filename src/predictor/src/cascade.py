1c530670bf5a7d82bc4aef166063df01cac2d05d
guillaume barree <guillaume.barree@student-cs.fr>
2021-11-22 22:14:29 +0100

    [~] Modify predictor structure for scaling

--- src/predictor/src/cascade.py
+++ src/predictor/src/cascade.py
@@ -6,26 +6,27 @@ class Cascade:
     
     def __init__(self, cid, producer_log) -> None:
         self.cid = cid
-        self.time_windows_list = []
+
         self.producer_log = producer_log
 
         self.tweet_msg = ''
 
-        self.windows = {}
-        self.status = 1  # 1 every thing alright / 0 waiting for partial cascade
+        self.time_window = None
 
-    def add_new_window(self, time_window):
-        if time_window not in self.windows.keys():
-            self.windows[time_window] = {
+        self.properties = {}
+
+    def add_properties(self):
+        if not self.properties:
+            self.properties = {
                 'parameters' : {},
                 'size': {}
             }
 
     def compute_w_reel(self, time_window):
-        n_star = self.windows[time_window]['parameters']['params']['n_star']
-        G1 = self.windows[time_window]['parameters']['params']['G1']
-        n_obs = self.windows[time_window]['parameters']['n_obs']
-        n_tot = self.windows[time_window]['size']['n_tot']
+        n_star = self.properties['parameters']['params']['n_star']
+        G1 = self.properties['parameters']['params']['G1']
+        n_obs = self.properties['parameters']['n_obs']
+        n_tot = self.properties['size']['n_tot']
         if G1==0:
             # Send log WARNING : G must not be equal to 0
             self.producer_log.send(
@@ -40,16 +41,16 @@ class Cascade:
             G1 = 1
         return (n_tot - n_obs) * (1 - n_star) / G1
 
-    def generate_sample_msg(self, time_window):
+    def generate_sample_msg(self):
         msg = {
             'type': 'sample',
             'cid': self.cid,
             'X' : [
-                self.windows[time_window]['parameters']['params']['beta'],
-                self.windows[time_window]['parameters']['params']['n_star'],
-                self.windows[time_window]['parameters']['params']['G1']
+                self.properties['parameters']['params']['beta'],
+                self.properties['parameters']['params']['n_star'],
+                self.properties['parameters']['params']['G1']
             ],
-            'W': self.windows[time_window]['size']['W']
+            'W': self.properties['size']['W']
         }
         return msg
 
@@ -59,18 +60,18 @@ class Cascade:
             'cid': self.cid,
             'msg': self.tweet_msg,
             'T_obs': time_window,
-            'n_supp_rf': self.windows[time_window]['parameters']['n_supp_rf'],
-            'n_supp': self.windows[time_window]['parameters']['n_supp']
+            'n_supp_rf': self.properties['parameters']['n_supp_rf'],
+            'n_supp': self.properties['parameters']['n_supp']
         }
         return msg
     
     def generate_stat_msg(self, time_window):
         # Compute ARE when predicting with RF
-        are_rf = abs(self.windows[time_window]['parameters']['n_supp_rf']-self.windows[time_window]['size']['n_tot']) / \
-            self.windows[time_window]['size']['n_tot']
+        are_rf = abs(self.properties['parameters']['n_supp_rf']-self.properties['size']['n_tot']) / \
+            self.properties['size']['n_tot']
         # Compute ARE only using hawkes estimator
-        are = abs(self.windows[time_window]['parameters']['n_supp']-self.windows[time_window]['size']['n_tot']) / \
-            self.windows[time_window]['size']['n_tot']
+        are = abs(self.properties['parameters']['n_supp']-self.properties['size']['n_tot']) / \
+            self.properties['size']['n_tot']
 
         msg = {
             'type': 'stat',
@@ -81,24 +82,24 @@ class Cascade:
         }
         return msg
 
-    def predict(self, time_window, model):
+    def predict(self, model):
         X = [
-                self.windows[time_window]['parameters']['params']['beta'],
-                self.windows[time_window]['parameters']['params']['n_star'],
-                self.windows[time_window]['parameters']['params']['G1']
+                self.properties['parameters']['params']['beta'],
+                self.properties['parameters']['params']['n_star'],
+                self.properties['parameters']['params']['G1']
             ]
-        n_supp_rf = self.windows[time_window]['parameters']['n_obs'] +                 \
+        n_supp_rf = self.properties['parameters']['n_obs'] +                 \
                  model.predict(X=np.array(X).reshape(1, -1))[0] *                   \
-                 self.windows[time_window]['parameters']['params']['G1'] /          \
-                 (1 - self.windows[time_window]['parameters']['params']['n_star'])
+                 self.properties['parameters']['params']['G1'] /          \
+                 (1 - self.properties['parameters']['params']['n_star'])
         
-        self.windows[time_window]['parameters']['n_supp_rf'] = n_supp_rf
+        self.properties['parameters']['n_supp_rf'] = n_supp_rf
 
-    def handle_parameters_type_msg(self, time_window, msg):
-        # Add new time window if doesn't exist
-        self.add_new_window(time_window=time_window)
+    def handle_parameters_type_msg(self, msg, time_window):
+        # Add properties if don't exist
+        self.add_properties()
 
-        self.windows[time_window]['parameters'] = {
+        self.properties['parameters'] = {
             'n_obs': msg['n_obs'],
             'n_supp': msg['n_supp'],
             'params': {
@@ -108,28 +109,26 @@ class Cascade:
             }
         }
 
-        self.tweet_msg=msg['msg']
+        self.time_window = time_window
+        self.tweet_msg = msg['msg']
     
-    def handle_size_type_msg(self, time_window, msg):
-        self.add_new_window(time_window=time_window)
+    def handle_size_type_msg(self, msg):
+        self.add_properties()
         
-        self.windows[time_window]['size'] = {
+        self.properties['size'] = {
             'n_tot': msg['n_tot'],
             't_end': msg['t_end']
         }
-        if not self.time_windows_list:
-            self.time_windows_list = msg['time_windows']
 
     def publish_sample_and_stat(self, producer):
-        for time_window in self.windows.keys():
 
-            self.windows[time_window]['size']['W'] = self.compute_w_reel(time_window)
+        self.properties['size']['W'] = self.compute_w_reel(self.time_window)
 
-            sample_msg = self.generate_sample_msg(time_window=time_window)
-            producer.send('samples', key = str(time_window), value = sample_msg)
+        sample_msg = self.generate_sample_msg()
+        producer.send('samples', key = str(self.time_window), value = sample_msg)
 
-            stat_msg = self.generate_stat_msg(time_window=time_window)
-            producer.send('stat', key = str(time_window), value = stat_msg)
+        stat_msg = self.generate_stat_msg(time_window=self.time_window)
+        producer.send('stat', key = str(self.time_window), value = stat_msg)
     
     def publish_alert(self, producer, time_window):
         alert_msg = self.generate_alert_msg(time_window=time_window)
@@ -137,10 +136,5 @@ class Cascade:
 
     def clean_memory(self):
         # Check if the cascade is over
-        is_finished = True
-        for time_window in self.time_windows_list:
-            if not (bool(self.windows.get(str(time_window), {}).get('size', False)) and bool(self.windows.get(str(time_window), {}).get('parameters', False))):
-                is_finished = False
-                break
-        return is_finished
+        return bool(self.properties['parameters'] and self.properties['size'])
 
